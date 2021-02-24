18074d8f6e096846dcb5bca452b4f51364fe2095
bertapsan <bertapsan@gmail.com>
2021-02-24 08:49:40 +0100

    Add first commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                Hola Mundo
+            </div>
+        );
+    }
+}
+export default App;
